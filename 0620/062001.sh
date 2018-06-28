e8416d4ec3c056b8ba4eddb5e72d780c7817f862
Nakasone Fumiya <n18019@std.it-college.ac.jp>
2018-06-28 04:54:48 +0000

    0620課題

--- 0620/062001.sh
+++ 0620/062001.sh
@@ -1,19 +1,17 @@
 #!/bin/bash
 
 for i in `seq 1 100`; do
-	 
- 	if [ $(($i % 3)) == 0 -a $(($i % 5)) == 0 ]; then
-			echo "FIZZBUZZ"
-    
-	elif [ $(($i % 3)) == 0 ]; then
-		  echo "FIZZ"
-		
-	elif [ $(($i % 5)) == 0 ]; then
-			echo "BUZZ"
-	
-	else
-			echo "$i"
-	
-	fi
-
+         
+    if [ $(($i % 3)) == 0 -a $(($i % 5)) == 0 ]; then
+        echo "FIZZBUZZ" | tr '\n' ','
+                            
+    elif [ $(($i % 3)) == 0 ]; then
+        echo "FIZZ" | tr '\n' ','
+                                                
+    elif [ $(($i % 5)) == 0 ]; then
+        echo "BUZZ" | tr '\n' ','
+                                                                
+    else
+        echo "$i" | tr '\n' ','
+    fi
 done
